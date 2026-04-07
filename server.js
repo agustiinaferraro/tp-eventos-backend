dd333a021f770fd765523aa9673839f4b06f797a
Agustina Ferraro <ferraroagustina25@gmail.com>
2026-04-07 00:03:29 -0300

    add cors middleware and json body parser

--- server.js
+++ server.js
@@ -2,12 +2,15 @@
 
 const express = require("express");
 const http = require("http");
+const cors = require("cors");
 const { Server } = require("socket.io");
 const config = require("./config");
 
 // CREACIÓN DEL SERVIDOR
 
 const app = express();
+app.use(cors());
+app.use(express.json({ limit: '50mb' }));
 const server = http.createServer(app);
 
 // CONFIGURACIÓN DE SOCKET.IO
