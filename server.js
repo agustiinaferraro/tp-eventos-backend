3392e7f247d9d80e8c44ebba1d11ec23f595ef29
Agustina Ferraro <ferraroagustina25@gmail.com>
2026-04-08 05:44:50 -0300

    Add explicit CORS headers for Vercel-Railway communication

--- server.js
+++ server.js
@@ -11,6 +11,15 @@ const config = require("./config");
 // CREACIÓN DEL SERVIDOR
 
 const app = express();
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") return res.sendStatus(200);
+  next();
+});
+
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 const server = http.createServer(app);
